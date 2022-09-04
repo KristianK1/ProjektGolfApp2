3cfd34a8b0b96a9f9615052451e497d0aad1c231
Kristian Klišković <kikihd11@gmail.com>
2022-09-04 15:37:18 +0200

    jkd -> 11, app isnt busy is device not configured

--- app/src/main/java/hr/ferit/kristiankliskovic/projektGolf/ui/mainFragment/MainFragment.kt
+++ app/src/main/java/hr/ferit/kristiankliskovic/projektGolf/ui/mainFragment/MainFragment.kt
@@ -178,6 +178,7 @@ class MainFragment: Fragment() {
             endInterval = null
             displayMarkers = false
             currCenterLocation = null
+            appIsBusy = false
             binding.centerView.setBackgroundColor(Color.RED);
             return
         }
